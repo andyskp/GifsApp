847f00853e078faac22cc6fd0179778206d11374
Andres <effectsxfighter@hotmail.com>
2025-07-25 13:56:29 -0500

    Se agregan app-side-menu-header como componente principal para el header y el app-side-menu-options para las opciones de navegacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,18 +3,25 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: 'dashboard',
-        loadComponent: () => import('./gifs/pages/dashboard-page/dashboard-page.component') //? Defecto
+        loadComponent: () => import('./gifs/pages/dashboard-page/dashboard-page.component'), //? Defecto
+        children: [
+
+            {
+                path: 'trending',
+                loadComponent: () => import('./gifs/pages/trending-page/trending-page.component') //? Defecto
+            },
+
+            {
+                path: 'search',
+                loadComponent: () => import('./gifs/pages/search-page/search-page.component') //? Defecto
+            },
+            {
+                path: '**',
+                redirectTo: 'trending' //!  Redirige a la página de trending si la ruta no coincide con ninguna de las anteriores
+            }
+        ]
     },
 
-    {
-        path: 'trending',
-        loadComponent: () => import('./gifs/pages/trending-page/trending-page.component') //? Defecto
-    },
-
-    {
-        path: 'search',
-        loadComponent: () => import('./gifs/pages/search-page/search-page.component') //? Defecto
-    },
     {
         path: '**',
         redirectTo: 'dashboard' //!  Redirige a la página de dashboard si la ruta no coincide con ninguna de las anteriores
---
+++ src/app/gifs/components/side-menu-options/side-menu-options.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gifs-app-side-menu-options',
+  imports: [],
+  templateUrl: './side-menu-options.component.html',
+})
+export class SideMenuOptionsComponent { }
