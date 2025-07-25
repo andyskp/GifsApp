58f347bdda9f8e01704388dd6e58dd7456df3d96
Andres <effectsxfighter@hotmail.com>
2025-07-25 11:28:14 -0500

    Se crean los componentes para las rutas del navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'dashboard',
+        loadComponent: () => import('./gifs/pages/dashboard-page/dashboard-page.component') //? Defecto
+    },
+
+    {
+        path: 'trending',
+        loadComponent: () => import('./gifs/pages/trending-page/trending-page.component') //? Defecto
+    },
+
+    {
+        path: 'search',
+        loadComponent: () => import('./gifs/pages/search-page/search-page.component') //? Defecto
+    },
+    {
+        path: '**',
+        redirectTo: 'dashboard' //!  Redirige a la página de dashboard si la ruta no coincide con ninguna de las anteriores
+    }
+];
