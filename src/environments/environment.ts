81c8bb6870a66da5c06d63d07a2cd6a4fd55e92e
Andres bustos <effectsxfighter@gmail.com>
2025-07-28 16:04:37 -0500

    Se crea una APIKey y se pone en las variables de entorno env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,9 +2,9 @@ export const environment = {
      production: true,
      companyName: 'Gifs',
      companyNames2: 'App2',
-     companySlogan: "Maneja tus gifs"
+     companySlogan: "Maneja tus gifs",
 
      //APIKeys
-
+     gifsApiKey: 'NvCxkEMCaYgddceHrgozV1hKtA6aUEJW'
      //URL
 };
