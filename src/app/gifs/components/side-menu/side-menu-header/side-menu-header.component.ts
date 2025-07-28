6bb3181ed74841bcc277d70b4b4b93473c685a52
Andres bustos <effectsxfighter@gmail.com>
2025-07-28 18:19:02 -0500

    Se corrigen importaciones al cambiar el nombre de los archivos

--- src/app/gifs/components/side-menu/side-menu-header/side-menu-header.component.ts
+++ src/app/gifs/components/side-menu/side-menu-header/side-menu-header.component.ts
@@ -1,5 +1,5 @@
 import {  Component } from '@angular/core';
-import { environment } from '@enviornments/environment.development';
+import { environment } from '@environments/environment';
 
 @Component({
   selector: 'gifs-app-side-menu-header',
